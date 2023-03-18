ac5e20ed20ebca9612ebbd925432e9e28e8315d9
felix1251 <domhack123@gmail.com>
2023-03-18 23:38:38 +0800

    add page properties

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,8 +1,22 @@
+import Head from 'next/head';
+
 function Users() {
-      return (<div className="text-red-800">Hi im felix</div>)
+      return (
+            <div className="text-red-800">
+                  <Head>
+                        <title>User Page</title>
+                        <meta name="description" content="User Page" />
+                  </Head>
+                  Hi im felix
+            </div>
+      )
 }
 
-Users.meta = { withLayout: true, module: "users" }
+Users.meta = {
+      withLayout: true,
+      module: "users",
+      name: "Users",
+}
 
 export default Users;
 
