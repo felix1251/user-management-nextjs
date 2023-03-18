60e2a660dff559edbf3d7d2e47a4d91cb05af4dd
felix1251 <domhack123@gmail.com>
2023-03-19 03:06:39 +0800

    .

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="description" content="User Management System" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
