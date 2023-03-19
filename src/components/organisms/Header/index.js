f826f15e4390df21c71996062524f24a34918027
felix1251 <domhack123@gmail.com>
2023-03-19 18:57:54 +0800

    add header component for auth layout

---
+++ src/components/organisms/Header/index.js
@@ -0,0 +1,11 @@
+const Header = () => {
+      return (
+            <header className="bg-white border-b shadow-sm">
+                  <div className="flex justify-end items-center max-w-7xl mx-auto h-14 px-2">
+                        <h2 className="font-medium text-md cursor-pointer">Logout</h2>
+                  </div>
+            </header>
+      )
+};
+
+export default Header;
--- src/components/templates/AuthLayout/index.js
+++ src/components/templates/AuthLayout/index.js
@@ -1,20 +1,14 @@
-import { Sidebar } from "@/organisms";
+import { Sidebar, Header } from "@/organisms";
 import { Container } from "@/atoms";
 
 function AuthLayout(children) {
       return (
             <div className="ml-72">
                   <Sidebar />
-                  <div className="w-full">
-                        <header className="bg-white border-b w-full shadow-sm">
-                              <div className="flex justify-end items-center max-w-7xl mx-auto h-14 px-2">
-                                    <h2 className="font-medium text-md cursor-pointer">Logout</h2>
-                              </div>
-                        </header>
-                        <Container>
-                              {children}
-                        </Container>
-                  </div>
+                  <Header />
+                  <Container>
+                        {children}
+                  </Container>
             </div>
       )
 }
